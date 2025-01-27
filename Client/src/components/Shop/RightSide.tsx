0b28fd978b189b9f4780ca20b0e5320bc99ce4dc
Shofiqul Islam <110051682+sujonahmedsr@users.noreply.github.com>
2025-01-28 01:38:29 +0600

    filterring dropdown added

--- Client/src/components/Shop/RightSide.tsx
+++ Client/src/components/Shop/RightSide.tsx
@@ -6,29 +6,31 @@ import {
     SelectItem,
     SelectTrigger,
     SelectValue,
-  } from "@/components/ui/select"
+} from "@/components/ui/select"
 import Cycle from "./Cycle";
+import SmallDeviceFiltering from "./SmallDeviceFiltering";
 
 const RightSide = () => {
     return (
         <div>
             <div className="flex md:flex-row flex-col gap-2 items-center justify-between">
                 <Input type="text" placeholder="Search your bi_cycle" className="md:w-2/5 w-full" />
-                <Select>
-                    <SelectTrigger className="md:w-[180px] w-full">
-                        <SelectValue placeholder="Sort By " />
-                    </SelectTrigger>
-                    <SelectContent>
-                        <SelectGroup>
-                            <SelectItem value="latest_product">Latest Product</SelectItem>
-                            <SelectItem value="old_product">Old Product</SelectItem>
-                            <SelectItem value="low_to_high">Low To High Price</SelectItem>
-                            <SelectItem value="high_to_low">High To Low Price</SelectItem>
-                        </SelectGroup>
-                    </SelectContent>
-
-                    
-                </Select>
+                <div className="flex items-center gap-5 w-full">
+                    <Select>
+                        <SelectTrigger className="md:w-[180px] w-full">
+                            <SelectValue placeholder="Sort By " />
+                        </SelectTrigger>
+                        <SelectContent>
+                            <SelectGroup>
+                                <SelectItem value="latest_product">Latest Product</SelectItem>
+                                <SelectItem value="old_product">Old Product</SelectItem>
+                                <SelectItem value="low_to_high">Low To High Price</SelectItem>
+                                <SelectItem value="high_to_low">High To Low Price</SelectItem>
+                            </SelectGroup>
+                        </SelectContent>
+                    </Select>
+                    <SmallDeviceFiltering />
+                </div>
             </div>
             <div className="grid lg:grid-cols-3 md:grid-cols-2 grid-cols-1 gap-5 py-5">
                 <Cycle />
--- Client/src/components/Shop/SmallDeviceFiltering.tsx
+++ Client/src/components/Shop/SmallDeviceFiltering.tsx
@@ -1,51 +1,23 @@
 import { Button } from "@/components/ui/button"
-import { Input } from "@/components/ui/input"
-import { Label } from "@/components/ui/label"
 import {
-  Sheet,
-  SheetClose,
-  SheetContent,
-  SheetDescription,
-  SheetFooter,
-  SheetHeader,
-  SheetTitle,
-  SheetTrigger,
+    Sheet,
+    SheetContent,
+    SheetTrigger,
 } from "@/components/ui/sheet"
+import LeftSide from "./LeftSide"
 
 const SmallDeviceFiltering = () => {
-  return (
-    <Sheet>
-      <SheetTrigger asChild>
-        <Button variant="outline">Open</Button>
-      </SheetTrigger>
-      <SheetContent>
-        <SheetHeader>
-          <SheetTitle>Edit profile</SheetTitle>
-          <SheetDescription>
-            Make changes to your profile here. Click save when you're done.
-          </SheetDescription>
-        </SheetHeader>
-        <div className="grid gap-4 py-4">
-          <div className="grid grid-cols-4 items-center gap-4">
-            <Label htmlFor="name" className="text-right">
-              Name
-            </Label>
-            <Input id="name" value="Pedro Duarte" className="col-span-3" />
-          </div>
-          <div className="grid grid-cols-4 items-center gap-4">
-            <Label htmlFor="username" className="text-right">
-              Username
-            </Label>
-            <Input id="username" value="@peduarte" className="col-span-3" />
-          </div>
+    return (
+        <div className="md:hidden block">
+            <Sheet>
+                <SheetTrigger asChild>
+                    <Button variant="outline">Filtering</Button>
+                </SheetTrigger>
+                <SheetContent>
+                    <LeftSide />
+                </SheetContent>
+            </Sheet>
         </div>
-        <SheetFooter>
-          <SheetClose asChild>
-            <Button type="submit">Save changes</Button>
-          </SheetClose>
-        </SheetFooter>
-      </SheetContent>
-    </Sheet>
-  )
+    )
 }
 export default SmallDeviceFiltering
